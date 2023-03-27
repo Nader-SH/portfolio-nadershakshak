c11ad78f937848ec96fe318592ff75a11dda0ecd
Nader-SH <nadershak149@gmail.com>
2023-03-27 13:24:58 +0300

    file structure and project setup

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      <div>
+        <h1> Contacts : </h1>
+      </div>
+      <div>
+        <a>instgram</a>
+        <a>email</a>
+        <a>Linkedin</a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

9d3db23fb416868ffea17aa1c6655ce769d3f5b4
Nader-SH <nadershak149@gmail.com>
2023-03-29 15:19:14 +0300

    fix The Data

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,20 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-    <Head>
-    <title>Nader | Homepage</title>
-    </Head>
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>
-        It is a long established fact that a reader will be distracted by the
-        readable content of a page when looking at its layout. The point of
-        using Lorem Ipsum is that it has a more-or-less normal distribution of
-        letters, as opposed to using Content here, content here, making it
-        look like readable English. Many desktop publishing packages and web
-        page editors now use Lorem Ipsum as their default model text, and a
-        search for lorem ipsum will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by
-        accident, sometimes on purpose (injected humour and the like).
-      </p>
-      <Link href="/nader" className={styles.btn}>
-        See Nader List
-      </Link>
-    </div>
+      <Head>
+        <title>Nader | Homepage</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Welcome to my personal website!</h1>
+        <p className={styles.text}>
+          My name is Nader Shakshak and I`m thrilled to have you here. This
+          website is a space for me to share my thoughts, experiences, and ideas
+          with the world.
+        </p>
+        <Link href="/nader" className={styles.btn}>
+          See Nader List
+        </Link>
+      </div>
     </>
   );
 }
